73bb7078225e909103bd9a0bab2379a68e726552
gitcmartel <martel.c73@gmail.com>
2024-05-07 14:17:29 +0200

    Create LoginController.php

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+
+    /**
+     * Disconnect a logged user
+     */
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        throw new \LogicException();
+    }
+}
