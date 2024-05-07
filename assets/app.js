d1cc8f499c556328790e471a9a2af06be7ceb6c5
gitcmartel <martel.c73@gmail.com>
2024-05-07 15:59:21 +0200

    Adding custom css file, bootstrap and jquery imports

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/shop-homepage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import $ from 'jquery';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
