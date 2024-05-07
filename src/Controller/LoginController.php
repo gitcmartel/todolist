3e55a7484da173359103b6bf57dcea9e0d667270
gitcmartel <martel.c73@gmail.com>
2024-05-07 16:50:42 +0200

    Adding Authentication error management

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils, Security $security): Response
     {
+        // logout the user in on the current firewall
+        if ($security->getUser()) {
+            $security->logout();
+        }
+
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error'         => $error
         ]);
     }
 
