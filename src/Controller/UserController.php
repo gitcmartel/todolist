321d1ca5c010557b01035e09f8b1ecac7f27b938
gitcmartel <martel.c73@gmail.com>
2024-05-16 15:34:01 +0200

    Adding comments and minor corrections

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,13 +12,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-
+/**
+ * The User controller.
+ *
+ * This controller allows you to create, list and edit application users.
+ */
 class UserController extends AbstractController
 {
     private UserRepository $userRepository;
     private EntityManagerInterface $manager;
     private UserPasswordHasherInterface $passwordHasher;
 
+    /**
+     * UserController constructor.
+     *
+     * @param EntityManagerInterface $manager Entity manager for ORM operations.
+     * @param UserRepository $userRepository Repository for user-related operations.
+     * @param UserPasswordHasherInterface $passwordHasher Password hashing service.
+     */
     public function __construct(EntityManagerInterface $manager, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher) 
     {
         $this->manager = $manager;
@@ -26,13 +37,25 @@ class UserController extends AbstractController
         $this->passwordHasher = $passwordHasher;
     }
 
+    /**
+     * Display the users list.
+     *
+     * @return Response Rendering the user list.
+     */
     #[Route('/users', name: 'app_user_list')]
-    public function listAction()
+    public function list()
     {
-        return $this->render('user/list.html.twig', ['users' => $this->userRepository->findAll()]);
+        return $this->render('user/list.html.twig', [
+            'users' => $this->userRepository->findAll()
+        ]);
     }
 
-
+    /**
+     * Creates a new user.
+     *
+     * @param Request $request HTTP request.
+     * @return Response Rendering the user creation.
+     */
     #[Route('/users/create', name: 'app_user_create')]
     public function create(Request $request) : Response
     {
@@ -40,7 +63,7 @@ class UserController extends AbstractController
         $form = $this->createForm(UserFormType::class, $user);
         
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
             $hashedPassword = $this->passwordHasher->hashPassword(
                 $user, 
@@ -59,19 +82,28 @@ class UserController extends AbstractController
             return $this->redirectToRoute('app_user_list');
         }
         
-        
         return $this->render('user/create.html.twig', [
             'controller_name' => 'UserController',
             'formUser' => $form
         ]);
     }
 
-
+    /**
+     * Édit an existing user.
+     *
+     * @param User $user The user to edit.
+     * @param Request $request HTTP request.
+     * @return Response Rendering the user edit.
+     */
     #[Route('/users/{id}/edit', name: 'app_user_edit')]
-    public function editAction(User $user, Request $request)
+    public function edit(User $user, Request $request)
     {
+        $currentRole = $user->getRoles()[0] ?? 'ROLE_USER';
 
-        $form = $this->createForm(UserFormType::class, $user);
+        $form = $this->createForm(UserFormType::class, $user, [
+            'include_password' => false, 
+            'current_role' => $currentRole
+        ]);
 
         $form->handleRequest($request);
         
@@ -82,13 +114,19 @@ class UserController extends AbstractController
             );
             $user->setPassword($hashedPassword);
 
+            $user->setRoles([$form->get('role')->getData()]);
+
             $this->manager->flush();
 
             $this->addFlash('success', "L'utilisateur a bien été modifié");
 
-            return $this->redirectToRoute('user_list');
+            return $this->redirectToRoute('app_user_list');
         }
 
-        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+        return $this->render('user/edit.html.twig', [
+            'controller_name' => 'UserController', 
+            'formUser' => $form, 
+            'user' => $user
+        ]);
     }
 }
