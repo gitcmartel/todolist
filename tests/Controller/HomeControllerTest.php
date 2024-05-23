47d89d67c9c2679ae0cbc4bea9371b177fb83b49
gitcmartel <martel.c73@gmail.com>
2024-05-23 19:29:25 +0200

    Create HomeControllerTest.php

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Bienvenue sur Todo List', $crawler->filter('h1')->text());
+    }
+}
