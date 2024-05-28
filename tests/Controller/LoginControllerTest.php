a439cb26a16123400b0b31b4bb6c8b2ea3b93ce4
gitcmartel <martel.c73@gmail.com>
2024-05-28 20:16:42 +0200

    Create LoginControllerTest.php

---
+++ tests/Controller/LoginControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Bienvenue sur Todo List', $crawler->filter('h1')->text());
+    }
+}
