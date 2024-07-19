886a7613a40310ee13cfe92f885c98308a4d2f0b
gitcmartel <martel.c73@gmail.com>
2024-07-19 11:34:16 +0200

    Adding testLogout route

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -7,13 +7,37 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\UserRepository;
+use App\Tests\Factory\UserFactory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class LoginControllerTest extends WebTestCase
 {
+    use ResetDatabase, Factories;
+
+    private $client;
+    private $userRepository;
+
+    protected function setUp(): void 
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get(UserRepository::class);
+    }
+
+    private function createAndLoginTestUser()
+    {
+        UserFactory::createOne([
+            'username' => 'usertest',
+            'roles' => ['ROLE_ADMIN']
+        ]);
+
+        $testUser = $this->userRepository->findOneByUsername('usertest');
+        $this->client->loginUser($testUser);
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
-
         $authenticationUtils = $this->createMock(AuthenticationUtils::class);
         $authenticationUtils->method('getLastAuthenticationError')
             ->willReturn(null);
@@ -26,16 +50,14 @@ class LoginControllerTest extends WebTestCase
             ->willReturn(null);
         self::getContainer()->set(Security::class, $security);
         
-        $crawler = $client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertStringContainsString('Nom d\'utilisateur', $crawler->filter('label')->text());
     }
 
     public function testUserIsLoggedOutIfAuthenticated()
     {
-        $client = static::createClient();
-
         $authenticationUtils = $this->createMock(AuthenticationUtils::class);
         $authenticationUtils->method('getLastAuthenticationError')
             ->willReturn(null);
@@ -51,13 +73,11 @@ class LoginControllerTest extends WebTestCase
         $security->expects(self::once())->method('logout');
         self::getContainer()->set(Security::class, $security);
 
-        $client->request('GET', '/login');
+        $this->client->request('GET', '/login');
     }
 
     public function testDataIsPassedToView()
     {
-        $client = static::createClient();
-
         $authenticationUtils = $this->createMock(AuthenticationUtils::class);
         $authenticationUtils->method('getLastAuthenticationError')
             ->willReturn(null);
@@ -70,10 +90,22 @@ class LoginControllerTest extends WebTestCase
             ->willReturn(null);
         self::getContainer()->set(Security::class, $security);
 
-        $crawler = $client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertCount(1, $crawler->filter('#username'));
         $this->assertStringContainsString('testUsername', $crawler->filter('#username')->attr('value'));
+    }
+
+    public function testLogout()
+    {
+        $security = static::getContainer()->get(Security::class);
+        $this->createAndLoginTestUser();
+        $this->assertEquals('usertest', $security->getUser()->getUsername());
+
+        $this->client->request('GET', '/logout');
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertNull($security->getUser());
     }
 }
